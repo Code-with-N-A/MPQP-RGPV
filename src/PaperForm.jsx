4213714ba4181fd841e315a62a9564aa57591550
codewithna <codewithna73@gmail.com>
2025-12-28 13:47:59 +0530

    update

--- src/PaperForm.jsx
+++ src/PaperForm.jsx
@@ -25,6 +25,7 @@ export default function PaperForm() {
 
   // Mapping Object for Full Forms
   const branchesMap = {
+    "1 & 2 sem":" All Branches",
     "CSE": "Computer Science Engineering",
     "CE": "Civil Engineering",
     "ME": "Mechanical Engineering",
